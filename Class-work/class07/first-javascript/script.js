94a3d23b72592cf571b3ba2b1fcebf6246108eef
Sarah Scott <sarahpatscott@gmail.com>
2017-12-11 14:27:50 -0500

    Final Project Idea

---
+++ Class-work/class07/first-javascript/script.js
@@ -0,0 +1,25 @@
+console.log('this is a sweet status update');
+
+var product = 10*10;
+
+var quotient = 100/10;
+
+var difference = 50-17;
+
+var sum = 50+17; 
+
+console.log('Sarah' + ' Scott');
+
+console.log(product + quotient + difference + sum + name);
+
+function onra () {
+  var city='Saigon';
+  
+  console.log(`Last Tango In ${city}`)
+}
+
+onra();
+onra();
+onra();
+
+console.log(document.getElementById("hello what's up"));
